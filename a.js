de0e5d5483ebdfd5d498ea7e6eceae150c0b85c1
PFG Danish <144151435+danishsheikh1122@users.noreply.github.com>
2023-11-08 17:04:01 +0530

    Add files via upload

---
+++ a.js
@@ -0,0 +1,43 @@
+// let val = document.getElementById("enter_number");
+// let displ = document.getElementById("chg_num");
+// let arr = [];
+// let arrr = [];
+// // const a = () => {
+// //     for (let i = 0; i <4; i++) {
+// //         let a = val.value;
+// //         arr = a.split("");
+// //         arrr[i] = [arr[i+0], arr[i+1], arr[i+2], arr[i+3]];
+// //         // arr=[];
+// //         // console.log(arrr[i]);
+// //         // console.log(arr);
+// //     }
+// //     console.log(arrr[0]);
+// //     console.log(arrr[1]);
+// //     // console.log(arrr[2]);
+// //     // console.log(arrr[3]);
+// // //   arrr[1] = [arr[0], arr[1], arr[2], arr[3]];
+// // //   arr=[];
+// // //   console.log(arrr[0]);
+// // //   console.log(arr);
+// // //   arrr[0] = [arr[0], arr[1], arr[2], arr[3]];
+// // };
+
+
+// let ab=[]
+
+
+
+// const a=()=>{
+//     let a = val.value;
+//     arr = a.split("");
+//     // console.log(arr);
+//     ab[0]=[arr[0],arr[1],arr[2],arr[3],[" "]]
+//     ab[1]=[arr[5],arr[6],arr[7],arr[8],[" "]]
+//     ab[2]=[arr[9],arr[10],arr[11],arr[12],[" "]]
+//     ab[3]=[arr[13],arr[14],arr[15],arr[16],[" "]]
+//     console.log(ab.join(""));
+
+// }
+// for(let i;i<=3;i++){
+//     displ.innerText=ab[i]
+// }
